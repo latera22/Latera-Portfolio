6955debd914baea7cd8a657c73827a70e808cf9f
Latera <laternigatu202@gmail.com>
2025-08-02 19:38:12 +0800

    vite

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  define: {
+    __DEFINES__: {},
+    __HMR_CONFIG_NAME__: JSON.stringify('Latera Portfolio') // This defines it properly at build time
+  }
 })
