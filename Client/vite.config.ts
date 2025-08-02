8c854c40b781782a14cbb1226de4a7a2d4f44d3b
Latera <laternigatu202@gmail.com>
2025-08-02 19:50:40 +0800

    new vite

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // Your base path
+  base: '/',
   plugins: [react()],
   define: {
     __DEFINES__: {},
-    __HMR_CONFIG_NAME__: JSON.stringify('some_value'),
-    __BASE__: JSON.stringify('/')  // Define __BASE__ as stringified base path
+    __HMR_CONFIG_NAME__: JSON.stringify('Latera Portfolio'),
+    __BASE__: JSON.stringify('/'),
+    __SERVER_HOST__: JSON.stringify('localhost'),
+    __HMR_PROTOCOL__: JSON.stringify('ws'),
+    __HMR_PORT__: JSON.stringify('5173'), // default vite port or your actual dev port
+    __HMR_HOSTNAME__: JSON.stringify('localhost'),
+    __HMR_BASE__: JSON.stringify('/'),
+    __HMR_DIRECT_TARGET__: JSON.stringify(''),
+    __WS_TOKEN__: JSON.stringify(''),
+     __HMR_ENABLE_OVERLAY__: JSON.stringify(true),
   }
 })
