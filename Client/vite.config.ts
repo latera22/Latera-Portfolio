9ffb9a9dabf243752c8b78aada1c15a351f6a461
Latera <laternigatu202@gmail.com>
2025-10-01 16:58:32 +0800

    new vite

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -5,4 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  define: {
+
+   __DEFINES__: {
+      'my.custom.setting': JSON.stringify('value'),
+   },
+   __BASE__: JSON.stringify('/'),
+    __HMR_CONFIG_NAME__: JSON.stringify("Latera Portfolio"),
+    __SERVER_HOST__: JSON.stringify("localhost"),
+    __HMR_PROTOCOL__: JSON.stringify("ws"),
+    __HMR_PORT__: JSON.stringify(""),
+    __HMR_HOSTNAME__: JSON.stringify("localhost"),
+    __HMR_BASE__: JSON.stringify("/"),
+    __HMR_DIRECT_TARGET__: JSON.stringify(""),
+    __WS_TOKEN__: JSON.stringify(""),
+    __HMR_ENABLE_OVERLAY__: JSON.stringify(false)
+  }
 })
